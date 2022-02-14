6aa00ebddd42ad685dca7197951b340eb71ca4fc
luzmmerlim <luziamerlim.1309@gmail.com>
2022-02-14 15:43:24 -0300

    exercicio if-else

---
+++ script.js
@@ -0,0 +1,21 @@
+let idade= 18;
+if (idade >=18){
+  console.log("Voce é maior de 18")
+} else{
+  console.log ("Voce não é maior de 18")
+}
+
+
+
+let amIhuman= true;
+if(idade == 18 && amIhuman == true){
+ console.log("Você é maior de idade e é humano")
+}
+
+
+let aniversário= "agosto"
+if(aniversário == "janeiro" || aniversário == "dezembro"){
+  console.log("Parabéns!")
+} else{
+  console.log("Espere mais um pouco")
+}
